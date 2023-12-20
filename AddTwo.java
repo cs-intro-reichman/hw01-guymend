6802f3f135f184c711fc163d470600bf6d636afd
guymend <guy.mendelson@post.runi.ac.il>
2023-12-20 17:06:16 +0200

    First attempt

--- AddTwo.java
+++ AddTwo.java
@@ -2,7 +2,7 @@
  * Adds two given integers and prints the result in a fancy way.
  */
 public class AddTwo {
-	public static void main(Int[] args) {
+	public static void main(String[] args) {
 		// Put your code here
 		int result = args[0]+args[1];	
 		System.out.println(args[0]+" + "+args[1]+" = "+result);
