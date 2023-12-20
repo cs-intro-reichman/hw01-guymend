ad728eb39c515829dd21561cae16e83af5c183ed
guymend <154328001+guymend@users.noreply.github.com>
2023-12-20 17:35:38 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -2,9 +2,9 @@
  * Adds two given integers and prints the result in a fancy way.
  */
 public class AddTwo {
-	public static void main(int[] args) {
+	public static void main(String[] args) {
 		// Put your code here
-		int result = args[0]+args[1];	
-		System.out.println(args[0]+" + "+args[1]+" = "+result);
+		int result = Integer.valueOf( args[0] ) + Integer.valueOf( args[1] );	
+		System.out.println(args[0] + " + " + args[1] + " = " + result);
 	}
 }
