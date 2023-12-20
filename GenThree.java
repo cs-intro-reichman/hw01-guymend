50bc29d8e60dfdcf56a7076f09321b9df206c7f4
guymend <154328001+guymend@users.noreply.github.com>
2023-12-20 17:33:30 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -3,14 +3,14 @@
  * prints them, and then prints the minimal number that was generated.
  */
 public class GenThree {
-	public static void main(String[] args) {
+	public static void main(int[] args) {
 		// Put your code here
 		int min = args[0]; 
 		int max = args[1];
 		int i = 0;
 		int numberArray[];
 		while( i < 3 ){
-	        	int numberArray[i] = (int)Math.floor( Math.random() * (max - min + 1) + min );
+	        	numberArray[i] = (int)Math.floor( Math.random() * (max - min + 1) + min );
 			System.out.println( numberArray[i] );
 			i = i + 1;
 		}
