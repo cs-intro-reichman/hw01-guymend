99644a98857b49a47dad7b9c39ad3bd42ccaee98
guymend <154328001+guymend@users.noreply.github.com>
2023-12-20 17:43:37 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -3,10 +3,10 @@
  * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
  */
 public class Coins {
-	public static void main(int[] args) {
+	public static void main(String[] args) {
 		// Put your code here
-		int quarters = args[0]/25;
-		int cents = args[0]%25;
+		int quarters = Integer.valueOf( args[0] )/25;
+		int cents = Integer.valueOf( args[0] )%25;
 		System.out.println("Use "+quarters+" quarters and "+cents+" cents");
 	}
 }
