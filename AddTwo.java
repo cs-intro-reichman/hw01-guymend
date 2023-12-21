ceb1a43620e1da99912da86e8ddd7a7976862b24
guymend <154328001+guymend@users.noreply.github.com>
2023-12-21 09:27:04 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -4,7 +4,7 @@
 public class AddTwo {
 	public static void main(String[] args) {
 		// Put your code here
-		int result = Integer.valueOf (args[0]) + Integer.valueOf (args[1]);	
+		int result = Integer.valueOf(args[0]) + Integer.valueOf(args[1]);	
 		System.out.println(args[0] + " + " + args[1] + " = " + result);
 	}
 }
