4f116806780cd4a1c63c8cb2b4221610ac0e9d2b
guymend <154328001+guymend@users.noreply.github.com>
2023-12-21 09:27:57 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -5,16 +5,16 @@
 public class GenThree {
 	public static void main(String[] args) {
 		// Put your code here
-		int min = Integer.valueOf( args[0] ); 
-		int max = Integer.valueOf( args[1] );
+		int min = Integer.valueOf(args[0]); 
+		int max = Integer.valueOf(args[1]);
 		int i = 0;
 		int[] numberArray = new int[3];
 		while( i < 3 ){
-	        	numberArray[i] = (int)( Math.random() * (max - min) + min );
+	        	numberArray[i] = (int)(Math.random() * (max - min) + min);
 			i = i + 1;
 		}
-		int minNumber = Math.min( numberArray[0], numberArray[1] );
-		minNumber = Math.min( numberArray[2], minNumber );
-		System.out.println( numberArray[0] + "\n" + numberArray[1] + "\n" + numberArray[2] + "\n" + "The minimal generated number was "+ minNumber );
+		int minNumber = Math.min(numberArray[0], numberArray[1]);
+		minNumber = Math.min(numberArray[2], minNumber);
+		System.out.println(numberArray[0] + "\n" + numberArray[1] + "\n" + numberArray[2] + "\n" + "The minimal generated number was "+ minNumber);
 	}
 }
